0901a02200cc874b490e45b8c3d1e59dd580f129
Elcan <elcanaliyev944@gmail.com>
2025-10-27 18:06:00 +0400

    Slagger elave etmek

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse  
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -36,3 +38,7 @@ class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+
+
--- shop/views.py
+++ shop/views.py
@@ -1,14 +1,32 @@
 from rest_framework import viewsets, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
+from django.shortcuts import render
 from .models import Product, Category, Order
 from .serializers import ProductSerializer, CategorySerializer, OrderSerializer
+from .filters import ProductFilter  
 
-class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+
+def home(request):
+    return render(request, 'shop/home.html')
+
+def orders_view(request):
+    orders = Order.objects.all().order_by('-created_at')
+    return render(request, 'shop/orders.html', {'orders': orders})
+
+
+class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    filterset_fields = ["category"]
+    filterset_class = ProductFilter  # min/max price + category filter
     search_fields = ["name", "description"]
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            permission_classes = [permissions.IsAdminUser]
+        else:
+            permission_classes = [permissions.AllowAny]
+        return [permission() for permission in permission_classes]
 
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
